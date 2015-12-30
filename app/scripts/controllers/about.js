8bcd7231e93ebcd19f33c720c7870a7873aafb4e
cHeBeee <che@10g.pl>
2015-12-30 16:18:11 +0100

    add project yo_angular

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yomenTestApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the yomenTestApp
+ */
+angular.module('yomenTestApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yomenTestApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the yomenTestApp
+ */
+angular.module('yomenTestApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
