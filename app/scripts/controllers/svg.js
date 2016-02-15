cfe2c3c72ed20b41da84f3b74879f8b8beaf75fb
cHeBeee <che@10g.pl>
2016-02-15 23:19:47 +0100

    svg round corner

--- app/scripts/controllers/svg.js
+++ app/scripts/controllers/svg.js
@@ -16,7 +16,7 @@ angular.module('yomenTestApp')
     ];
     
     $scope.strokeRound = 8;
-    $scope.strokeWidth = 12;
+    $scope.strokeWidth = 0;
 
      $scope.getRoundOne = function(round) {
        // return parseInt(round) + ($scope.strokeWidth / (parseInt(round)/2));
@@ -30,7 +30,6 @@ angular.module('yomenTestApp')
       } else {
         val = round - (size/2);
       }
-      console.info(val);
       return val;
     };
 
@@ -60,19 +59,74 @@ angular.module('yomenTestApp')
 
 
 
-    $scope.roundedPath = 'M0 0 L 64 0 L64 64 L 0 64 Z';
+    // $scope.roundedPath = 'M0 0 L 64 0 L64 64 L 0 64 Z';
 
     $scope.roundingTestCase = 'M0 0 L 64 0 L64 64 L 0 64 Z';
+    $scope.roundingTestCaseM = 'M0 0 L 64 0 L64 64 L 0 64 Z';
     $scope.config = {
       rounding: 0.1,
-      fractional: false
+      fractional: false,
+      inner: 62
     };
 
-    /* jshint ignore:start */
-    $scope.roundedPath = roundPathCorners(
-      $scope.roundingTestCase, 
-      $scope.config.fractional ? $scope.config.rounding : $scope.config.rounding * 100,
-      $scope.config.fractional
+
+    //http://stackoverflow.com/questions/12115691/svg-d3-js-rounded-corner-on-one-corner-of-a-rectangle/12124192#12124192
+     $scope.rightRoundedRectTwo = function (x, y, w, h, r, tl, tr, bl, br) {
+      var retval;
+      retval  = "M" + (x + r) + "," + y;
+      retval += "h" + (w - 2*r);
+      if (tr) { retval += "a" + r + "," + r + " 0 0 1 " + r + "," + r; }
+      else { retval += "h" + r; retval += "v" + r; }
+      retval += "v" + (h - 2*r);
+      if (br) { retval += "a" + r + "," + r + " 0 0 1 " + -r + "," + r; }
+      else { retval += "v" + r; retval += "h" + -r; }
+      retval += "h" + (2*r - w);
+      if (bl) { retval += "a" + r + "," + r + " 0 0 1 " + -r + "," + -r; }
+      else { retval += "h" + -r; retval += "v" + -r; }
+      retval += "v" + (2*r - h);
+      if (tl) { retval += "a" + r + "," + r + " 0 0 1 " + r + "," + -r; }
+      else { retval += "v" + -r; retval += "h" + r; }
+      retval += "z";
+      return retval;
+    }
+
+    $scope.updateRoudn = function() {
+
+      $scope.config.rounding = ($scope.strokeRound - 0)/(32-0);
+
+      /* jshint ignore:start */
+      $scope.roundedPath = roundPathCorners(
+        $scope.roundingTestCase, 
+        // $scope.config.fractional ? $scope.config.rounding : $scope.config.rounding * 30,
+        $scope.strokeRound,
+        $scope.config.fractional
       );
-    /* jshint ignore:end */
+
+      $scope.roundingTestCaseM = 'M0 0 L ' + $scope.config.inner + ' 0 L' + $scope.config.inner + ' ' + $scope.config.inner + ' L 0 ' + $scope.config.inner + ' Z';
+
+      $scope.roundedPathM = roundPathCorners(
+        $scope.roundingTestCaseM, 
+        ($scope.config.rounding * 30) - $scope.strokeWidth,
+        $scope.config.fractional
+      );
+      /* jshint ignore:end */
+
+      $scope.roundedPathTwo = $scope.rightRoundedRectTwo(0, 0, 64, 64, $scope.strokeRound, 100, 100, 100, 100);
+
+    };
+    $scope.updateRoudn();
+
+
+    $scope.rightRoundedRect = function(x, y, width, height, radius) {
+      return "M" + x + "," + y
+          + "h" + (width - radius)
+          + "a" + radius + "," + radius + " 0 0 1 " + radius + "," + radius
+          + "v" + (height - 2 * radius)
+          + "a" + radius + "," + radius + " 0 0 1 " + -radius + "," + radius
+          + "h" + (radius - width)
+          + "a" + radius + "," + radius + " 0 0 1 " + radius + "," + -radius
+          + "v" + (height - 2 * radius)
+          + "z";
+    };
+
   });
